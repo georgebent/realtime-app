2e8ddead3b9d8c9cefc17bc90dc3235dda887c2c
George Bent <gdevyur@gmail.com>
2019-01-12 14:05:46 +0200

    Fixes :: User to Models/User, home controller

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@home')->name('home-page');
+Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
